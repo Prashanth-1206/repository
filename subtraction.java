b7c97469195a781d4bf4edb4f2d23404de952492
Prashanth-1206 <159251164+Prashanth-1206@users.noreply.github.com>
2024-02-13 10:25:49 +0530

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class subtraction{
+  public static void main(String[]args){
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a:");
+    a=sc.nextInt();
+    System.out.println("enter b:");
+    b=sc.nextInt();
+    System.out.println("a-b="+(a-b));
+  }
+}
