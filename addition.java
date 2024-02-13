1ce0195b498448c014b820d116d63ab45b964091
Prashanth-1206 <159251164+Prashanth-1206@users.noreply.github.com>
2024-02-13 09:58:21 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class addition{
+  public static void main(String[]args){
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a:");
+    a=sc.nextInt();
+    System.out.println("enter b:");
+    b=sc.nextInt();
+    System.out.println("a+b="+(a+b));
+  }
+}
+    
