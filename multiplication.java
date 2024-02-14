11664b570e31b29d58ebdd7b4e4d26321fc798d6
Prashanth-1206 <159251164+Prashanth-1206@users.noreply.github.com>
2024-02-14 11:36:15 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ad{
+  public static void main(String[]args){
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a:");
+    a=sc.nextInt();
+    System.out.println("enter b:");
+    b=sc.nextInt();
+    System.out.println("axb="+(a*b));
+  }
+}
